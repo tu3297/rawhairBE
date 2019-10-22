cbce165fd4afb341d29ae2c82dd2946057ce269e
tu3297 <tu3297@gmail.com>
2019-10-22 17:24:32 +0700

    update pt

--- src/main/java/com/myproject/learn/controller/SizeController.java
+++ src/main/java/com/myproject/learn/controller/SizeController.java
@@ -1,8 +1,10 @@
 package com.myproject.learn.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SizeController {
-
+     @GetMapping(value="getAllSize")
+     public ResponseEntity
 }
